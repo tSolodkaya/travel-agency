cf6504028562b8cfc70efe96e3d784575386ab61
tSolodkaya <solodkayagit@gmail.com>
2023-09-15 22:36:42 +0200

    feat: add slider for galery, experience, ,offers

--- js/slider.js
+++ js/slider.js
@@ -21,19 +21,63 @@ $(".destination-slider").slick({
 });
 
 $(".offers-slider").slick({
-  prevArrow: "button[data-prev-destinations]",
-  nextArrow: "button[data-next-destinations]",
-  slidesToShow: 1,
-  slidesToScroll: 1,
+  prevArrow: "button[data-prev-offers]",
+  nextArrow: "button[data-next-offers]",
+  slidesToShow: 3,
+  slidesToScroll: 3,
   infinite: true,
   responsive: [
     {
       breakpoint: 1152,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 4,
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
+    },
+    {
       breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".galery-slider").slick({
+  prevArrow: "button[data-prev-galery]",
+  nextArrow: "button[data-next-galery]",
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  infinite: true,
+
+  responsive: [
+    {
+      breakpoint: 1152,
+      settings: {
+        vertical: true,
+      },
+    },
+  ],
+});
+
+$(".expirience-slider").slick({
+  prevArrow: "button[data-prev-exp]",
+  nextArrow: "button[data-next-exp]",
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  infinite: true,
+
+  responsive: [
+    {
+      breakpoint: 1152,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
